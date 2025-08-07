af4ea588407cae1c486e0ea0d5af5c407dab2289
NEFORMAL4IK <goblinesy@gmail.com>
2025-08-07 21:34:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import './App.css';
-import React, { useState } from 'react';
 
-function App() {
-  const [password, setPassword] = useState('');
-
-  const handleChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleClick = () => {
-    if (password === '32645') {
-      alert('Правильно бро');
-    } else {
-      alert('Неправильный пароль');
-    }
-  };
-
+function Head(){
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Приветствую</h1>
-        <p>Введите пароль</p>
-        <input
-          type="password"
-          className="inp"
-          value={password}
-          onChange={handleChange}
-        />
-        <button className="btn" onClick={handleClick}>Accept</button>
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default Head;
